e307c8fb5257367f8df5db8d9c15476f8175db0c
WizardXL <1272701292@qq.com>
2018-02-17 19:10:18 +0800

    update linearList

--- ArrayList/arrayList.c
+++ ArrayList/arrayList.c
@@ -21,24 +21,40 @@ typedef struct Linear {
 	USER_TYPE *arrays; 
 } Linear;
 
-boolean initLinearList(Linear **pLinear, int capacity); 
-boolean destroyLinearList(Linear **pLinear);
-void iterator(Linear linear, void (*operatorValue)(USER_TYPE value)); 
-void printOneElement(Point point);
-boolean resetLinearList(Linear *linear);
-int getLength(Linear linear);
-boolean isEmpty(Linear linear);
-boolean isFull(Linear linear);
-boolean append(Linear *linear, USER_TYPE value);
-boolean deleteElementByIndex(Linear *linear, int index);
-boolean getElementByIndex(Linear *linear, int index, USER_TYPE *value);
-boolean modifyElementByIndex(Linear *linear, int index, USER_TYPE value);
-boolean insertElement(Linear *linear, int index, USER_TYPE value);
-int indexOf(Linear linear, USER_TYPE value, boolean (*equals)(USER_TYPE one, USER_TYPE two)); 
-void bubbleSortList(Linear *linear, boolean isAscending,                     	
-	boolean (*oneGreaterThanTwo)(USER_TYPE one, USER_TYPE two));     
-boolean oneGreaterThanTwo(Point one, Point two);    
+boolean initLinearList(Linear **pLinear, int capacity); 					//初始化线性表						
+boolean destroyLinearList(Linear **pLinear);								//销毁线性表 
+void iterator(Linear linear, void (*operatorValue)(USER_TYPE value)); 		//对列表进行迭代 
+boolean resetLinearList(Linear *linear);									//重置线性表 
+int getLength(Linear linear);												//得到线性表长度 
+boolean isEmpty(Linear linear);												//判空 
+boolean isFull(Linear linear);												//判满
+boolean append(Linear *linear, USER_TYPE value);							//追加 
+boolean deleteElementByIndex(Linear *linear, int index);					//删除 
+boolean getElementByIndex(Linear *linear, int index, USER_TYPE *value);		//查看 
+boolean modifyElementByIndex(Linear *linear, int index, USER_TYPE value);	//修改
+boolean insertElement(Linear *linear, int index, USER_TYPE value);			//插入 
+int indexOf(Linear linear, USER_TYPE value, 								//得到第一个匹配的元素下标 
+	boolean (*equals)(USER_TYPE one, USER_TYPE two)); 		
+void bubbleSortList(Linear *linear, boolean isAscending,    				//排序                 	
+	boolean (*oneGreaterThanTwo)(USER_TYPE one, USER_TYPE two));     	
 
+void printOneElement(Point point);											//用户实现的打印一个元素 函数 
+boolean oneGreaterThanTwo(Point one, Point two);    						//用户实现的比较大小 	
+boolean equals(Point one, Point two);										//用户实现的比较是否相等 
+
+/*
+ 用户实现的打印一个元素
+ point:点坐标 
+*/
+void printOneElement(Point point) {
+	printf("(%d,%d)\n", point.row, point.col);
+}
+
+/*
+ 由用户实现的比较大小函数，若one > two，返回TRUE
+ one:待比较数据 
+ two:待比较数据 
+*/  
 boolean oneGreaterThanTwo(Point one, Point two) {
 	if (((one.row*one.row + one.col*one.col) 
 		- (two.row*two.row + two.col*two.col)) > 0) {
@@ -47,6 +63,24 @@ boolean oneGreaterThanTwo(Point one, Point two) {
 	return FALSE;
 }
 
+/*
+ 用户定义的比较相等函数 
+ one:待比较数据 
+ two:待比较数据 
+*/
+boolean equals(Point one, Point two) {
+	if (one.row	== two.row && one.col == two.col) {
+		return TRUE;
+	}	
+	return FALSE; 
+}
+
+/*
+ 进行冒泡排序
+ linear:指向线性表的指针
+ isAscending:是否升序。TRUE为升序，FALSE为降序
+ oneGreaterThanTwo:比较大小函数 
+*/
 void bubbleSortList(Linear *linear, boolean isAscending,                    
 	boolean (*oneGreaterThanTwo)(USER_TYPE one, USER_TYPE two)) {
 	int i,j;
@@ -67,6 +101,12 @@ void bubbleSortList(Linear *linear, boolean isAscending,
 	}		
 } 
 
+/*
+ 得到匹配的第一个元素下标
+ linear:线性表实例
+ value:待查找元素
+ equals：比较函数 
+*/
 int indexOf(Linear linear, USER_TYPE value, boolean (*equals)(USER_TYPE one, USER_TYPE two)) {
 	int len = linear.count;
 	
@@ -78,6 +118,12 @@ int indexOf(Linear linear, USER_TYPE value, boolean (*equals)(USER_TYPE one, USE
 	return NOT_FOUND;
 }
 
+/*
+ 向指定位置插入数据 
+ linear:指向线性表的指针
+ index:插入位置
+ value:插入数据 
+*/
 boolean insertElement(Linear *linear, int index, USER_TYPE value) {
 	int i; 
 	
@@ -94,6 +140,12 @@ boolean insertElement(Linear *linear, int index, USER_TYPE value) {
 	return TRUE;
 } 
 
+/*
+ 修改指定位置元素 
+ linear:指向线性表的指针 
+ index:修改位置
+ value:修改的元素 
+*/
 boolean modifyElementByIndex(Linear *linear, int index, USER_TYPE value) {
 	if (linear == NULL || TRUE == isEmpty(*linear)
 		|| index >= getLength(*linear) || index < 0) {
@@ -103,6 +155,12 @@ boolean modifyElementByIndex(Linear *linear, int index, USER_TYPE value) {
 	return TRUE;
 }
 
+/*
+ 查看指定位置元素
+ linear:指向线性表的指针 
+ index:查看位置
+ value:返回的数据 
+*/
 boolean getElementByIndex(Linear *linear, int index, USER_TYPE *value) {
 	if (linear == NULL || TRUE == isEmpty(*linear)
 		|| index >= getLength(*linear) || index < 0) {
@@ -113,6 +171,11 @@ boolean getElementByIndex(Linear *linear, int index, USER_TYPE *value) {
 	return TRUE;
 } 
 
+/*
+ 删除指定位置元素
+ linear:指向线性表的指针 
+ index:删除元素下标 
+*/
 boolean deleteElementByIndex(Linear *linear, int index) {
 	int i;
 	if (linear == NULL || TRUE == isEmpty(*linear)
@@ -127,6 +190,11 @@ boolean deleteElementByIndex(Linear *linear, int index) {
 	return TRUE;
 }
 
+/*
+ 追加元素
+ linear:指向线性表的指针 
+ value:待追加元素  
+*/
 boolean append(Linear *linear, USER_TYPE value) {
 	if (linear == NULL || TRUE == isFull(*linear)) {
 		return FALSE;
@@ -135,18 +203,34 @@ boolean append(Linear *linear, USER_TYPE value) {
 	return TRUE;
 }
 
+/*
+ 判满
+ linear:线性表实例 
+*/
 boolean isFull(Linear linear) {
 	return linear.count == linear.capacity;
 }
 
+/*
+ 判空 
+ linear:线性表实例 
+*/
 boolean isEmpty(Linear linear) {
 	return linear.count == 0;
 }
 
+/*
+ 得到线性表长度 
+ linear:线性表实例 
+*/
 int getLength(Linear linear) {
 	return linear.count;
 }
 
+/*
+ 重置线性表
+ linear:指向线性表的指针 
+*/
 boolean resetLinearList(Linear *linear) {
 	if (linear == NULL) {
 		return FALSE;
@@ -155,18 +239,23 @@ boolean resetLinearList(Linear *linear) {
 	return TRUE;
 }
 
-void printOneElement(Point point) {
-	printf("(%d,%d)\n", point.row, point.col);
-}
-
+/*
+ 对线性表进行迭代 
+ linear:线性表实例
+ operatorValue:对元素操作的函数 
+*/
 void iterator(Linear linear, void (*operatorValue)(USER_TYPE value)) {
-	int len = linear.capacity;
+	int len = linear.count;
 	
-	while (linear.capacity > 0) {
-		operatorValue(linear.arrays[len - linear.capacity--]);
+	while (linear.count > 0) {
+		operatorValue(linear.arrays[len - linear.count--]);
 	}
 }
 
+/*
+ 销毁链表，释放所有空间
+ linear:指向线性表指针的地址 
+*/ 
 boolean destroyLinearList(Linear **pLinear) {
 	if (*pLinear == NULL) {
 		return FALSE;
@@ -178,6 +267,11 @@ boolean destroyLinearList(Linear **pLinear) {
 	return TRUE;
 }
 
+/*
+ 初始化线性表 
+ linear:指向线性表指针的地址
+ capacity:容量GREFFFDXC 
+*/
 boolean initLinearList(Linear **pLinear, int capacity) {
 	if (*pLinear != NULL) {
 		return FALSE;
@@ -198,11 +292,16 @@ int main(void) {
 		1
 	};
 	
-	initLinearList(&linear, 10);
+	initLinearList(&linear, 20);
 	remainCapacity = 10 - getLength(*linear); 
-	while (remainCapacity-- > 0) { 					//填充线性表 
+	while (remainCapacity-- > 0) { 
+		one.row = one.col= remainCapacity;						//填充线性表 
 		append(linear, one);
 	}
+	append(linear, one);
+	insertElement(linear, 0, one);
+	//modifyElementByIndex(linear, 1, one);
+	//bubbleSortList(linear, TRUE, oneGreaterThanTwo); 
 	iterator(*linear, printOneElement);
 	
 	printf("当前线性表长度:[%d]\n" , getLength(*linear));
